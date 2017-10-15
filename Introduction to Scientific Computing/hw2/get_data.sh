5a429ffcd3b4ca2d2439ab2e377a5b32ba0c53f7
Dmitriy Salnikov <salnikov.dmitri@gmail.com>
2017-10-16 02:08:16 +0300

    added task2 and task5

---
+++ Introduction to Scientific Computing/hw2/get_data.sh
@@ -0,0 +1,7 @@
+mkdir data
+
+wget -P ./data https://archive.ics.uci.edu/ml/machine-learning-databases/00265/CASP.csv
+
+wget -P ./data/ https://archive.ics.uci.edu/ml/machine-learning-databases/00206/slice_localization_data.zip
+
+unzip ./data/slice_localization_data.zip
